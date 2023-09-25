3d45a8c69208124e62764aafbffa4f0e377a3964
W0810531 <W0810531@ADL-A0218Z-28>
2023-09-25 15:33:36 -0400

    Add project files.

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{ 
+
+public class HelloWorldController : Controller
+{
+    // 
+    // GET: /HelloWorld/
+   /* public string Index()
+    {
+        return "This is my default action...";
+    }*/
+
+        //Here is part 3, adding a view by replacing the default index method above
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // 
+        // GET: /HelloWorld/Welcome/ 
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Deposit to: " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+
+    }
+}
